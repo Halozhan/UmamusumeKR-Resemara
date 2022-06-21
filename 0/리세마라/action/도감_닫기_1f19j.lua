8908df85fe2a6edce9f78f2ffcf88592d2fe71cb
Halozhan <juwonhwang0411@gmail.com>
2022-06-21 16:56:10 +0900

    게스트 후 카카오 연동 후 계정 삭제 방식으로 변경

--- 0/리세마라/action/도감_닫기_1f19j.lua
+++ 0/리세마라/action/도감_닫기_1f19j.lua
@@ -1,3 +1,4 @@
 EnableImage(false, "메뉴로")
+EnableImage(false, "도감열기")
 EnableImage(true, "뽑기_이동")
 read_book = true
--- 0/리세마라/action/쥬얼_부족_jhji1.lua
+++ 0/리세마라/action/쥬얼_부족_jhji1.lua
@@ -1,7 +1,10 @@
 -- 카카오판 초기화
 if read_book then
-  EnableImage(true, "카카오_초기화_활성화")
+  EnableImage(false, "뽑기_이동")
+  EnableImage(true, "메뉴로")
+  EnableImage(true, "계정_정보")
 else
   EnableImage(false, "뽑기_이동")
   EnableImage(true, "메뉴로")
+  EnableImage(true, "도감열기")
 end
