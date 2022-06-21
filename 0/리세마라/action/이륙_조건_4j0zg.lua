7a2016728ffa111271fd1611d934707249f9cb01
Halozhan <juwonhwang0411@gmail.com>
2022-06-22 00:31:04 +0900

    이륙 조건과 미션 안받기로 변경

--- 0/리세마라/action/이륙_조건_4j0zg.lua
+++ 0/리세마라/action/이륙_조건_4j0zg.lua
@@ -1,5 +1,5 @@
--- 히비키 이오리
-if hibiki and iori then
+-- 파인 타즈나 슈퍼크릭
+if motion and creek and tazuna then
  SendKakaoTalk(0, "", true)
  Stop()
 end
--- 0/리세마라/action/초기화_완료_nyyvi.lua
+++ 0/리세마라/action/초기화_완료_nyyvi.lua
@@ -1,1 +1,6 @@
-EnableImage(false, "초기화_완료")
+-- 튜토리얼 끄기
+EnableImage(false, "튜토리얼1")
+EnableImage(false, "튜토리얼2")
+EnableImage(false, "튜토리얼3")
+EnableImage(false, "튜토리얼4")
+EnableImage(false, "튜토리얼5")
