82565236a7e1046cd4bc1eb6daceab03674ace96
Halozhan <juwonhwang0411@gmail.com>
2022-06-23 19:19:08 +0900

    카카오 연동 확인하기 이미지 더 추가, 이륙조건 SSR 6장 이상, 도감 열기, 미션 받기

---
+++ 0/리세마라/action/선물_들어가기_exorx.lua
@@ -0,0 +1,3 @@
+EnableImage(false, "메뉴로")
+EnableImage(false, "도감열기")
+EnableImage(false, "계정_정보")
--- 0/리세마라/action/이륙_조건_w24w9.lua
+++ 0/리세마라/action/이륙_조건_w24w9.lua
@@ -1,8 +1,8 @@
--- SSR 개수 5개 이상
+-- SSR 개수 6개 이상
 if SSR_count == nil then
   SSR_count = 0
 end
-if SSR_count >= 5 then
+if SSR_count >= 6 then
  SendKakaoTalk(0, "", true)
  Stop()
 end
