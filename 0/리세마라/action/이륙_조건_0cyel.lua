5958bb4249cef27775a6f005879e10e153ff7e7c
Halozhan <juwonhwang0411@gmail.com>
2022-06-24 23:42:54 +0900

    이미지 서칭 빨간 박스 대부분 숨김

--- 0/리세마라/action/이륙_조건_0cyel.lua
+++ 0/리세마라/action/이륙_조건_0cyel.lua
@@ -1,7 +1,7 @@
 -- SSR 개수 6개 이상 및
--- 파인모션 1장 이상 및
+-- 슈퍼크릭 1장 이상 및
 -- 타즈나 1장 이상
-if SSR_count >= 6 and motion >= 1 and tazuna >=1 then
+if SSR_count >= 6 and creek >= 1 and tazuna >=1 then
  SendKakaoTalk(0, "", true)
  Stop()
 end
---
+++ 0/리세마라/action/이륙_조건_49mbr.lua
@@ -0,0 +1,5 @@
+-- 슈퍼크릭 타즈나 비코
+if creek >= 1 and tazuna >= 1 and pegasus >= 1 then
+ SendKakaoTalk(0, "", true)
+ Stop()
+end
