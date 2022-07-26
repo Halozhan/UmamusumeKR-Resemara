d9899cd7a25ad3bb936dc72525c520b6c4cc724c
Halozhan <juwonhwang0411@gmail.com>
2022-07-26 23:12:47 +0900

    Feat: adbInput.py 예외처리에 재시도 추가

--- adbInput.py
+++ adbInput.py
@@ -28,8 +28,7 @@ def RandomPosition(x, y, deltaX, deltaY):
         y = random.randint(y - deltaY, y + deltaY)
     
         return x, y
-    except:
-        pass
+    except: pass
 
 def BlueStacksOffset(x, y): # 블루스택 이미지 서칭에서 가져온 위치로 터치하기 위해 블루스택 좌표로 변환
     try:
@@ -37,8 +36,7 @@ def BlueStacksOffset(x, y): # 블루스택 이미지 서칭에서 가져온 위�
         y -= 33
     
         return x, y
-    except:
-        pass
+    except: pass
 
 def Offset(x, y, offsetX, offsetY):
     x += offsetX
@@ -50,25 +48,37 @@ def shell(device: AdbClient.device, InstancePort, shell: str):
         device.shell(shell)
     except:
         AdbConnect(InstancePort)
+        try:
+            device.shell(shell)
+        except: pass
 
 def AdbTap(device: AdbClient.device, InstancePort, x, y): # 0초 동안 누름
     try:
         device.shell("input touchscreen tap " + str(x) + " " + str(y))
     except:
         AdbConnect(InstancePort)
+        try:
+            device.shell("input touchscreen tap " + str(x) + " " + str(y))
+        except: pass
 
 def AdbSwipe(device: AdbClient.device, InstancePort, x, y, toX, toY, delay): # 딜레이를 줘서 누름
     try:
         device.shell("input swipe " + str(x) + " " + str(y) + " " + str(toX) + " " + str(toY) + " " + str(delay))
     except:
         AdbConnect(InstancePort)
+        try:
+            device.shell("input swipe " + str(x) + " " + str(y) + " " + str(toX) + " " + str(toY) + " " + str(delay))
+        except: pass
 
 def Key_event(device: AdbClient.device, InstancePort, key_code:str):
     try:
         device.shell("input " + str(key_code))
     except:
         AdbConnect(InstancePort)
-
+        try:
+            device.shell("input " + str(key_code))
+        except: pass
+    
 def BlueStacksTap(device: AdbClient.device, InstancePort, position, offsetX = 0, offsetY = 0, deltaX = 0, deltaY = 0):
     try:
         x, y, width, height = position
@@ -80,7 +90,16 @@ def BlueStacksTap(device: AdbClient.device, InstancePort, position, offsetX = 0,
         AdbTap(device, InstancePort, x, y)
         return True
     except:
-        AdbConnect(InstancePort)
+        try:
+            AdbConnect(InstancePort)
+            x, y, width, height = position
+            x += width/2
+            y += height/2
+            x, y = BlueStacksOffset(x, y)
+            x, y = Offset(x, y, offsetX, offsetY)
+            x, y = RandomPosition(x, y, deltaX, deltaY)
+            AdbTap(device, InstancePort, x, y)
+        except: pass
         return False
 
 def BlueStacksSwipe(device: AdbClient.device, InstancePort, position, offsetX = 0, offsetY = 0, deltaX = 0, deltaY = 0):
@@ -95,6 +114,13 @@ def BlueStacksSwipe(device: AdbClient.device, InstancePort, position, offsetX = 
         return True
     except:
         AdbConnect(InstancePort)
+        x, y, width, height = position
+        x += width/2
+        y += height/2
+        x, y = BlueStacksOffset(x, y)
+        x, y = Offset(x, y, offsetX, offsetY)
+        x, y = RandomPosition(x, y, deltaX, deltaY)
+        AdbSwipe(device, InstancePort, x, y, x, y, random.randint(25, 75))
         return False
 
 if __name__ == "__main__":
